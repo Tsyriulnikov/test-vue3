71632b0078e76bb382f57baa10e75e06ccc41976
Tsyriulnikov <max_djvu@mail.ru>
2022-09-09 21:08:21 +0300

    lesson4_1

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,99 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<div class="container mt-2">
+		<form @submit.prevent="sendForm" v-if="!formDone">
+			<app-progress :current="fieldDone" :max="info.length" />
+			<div>
+				<app-field 
+					v-for="field,i in info"
+					:key="i"
+					:label="field.label"
+					:value="field.value"
+					:valid="field.valid"
+					@updated="onUpdate(i, $event)"
+				></app-field>
+			</div>
+			<button class="btn btn-primary" :disabled="!formReady">
+				Send Data
+			</button>
+		</form>
+		<div v-else>
+			<table class="table table-bordered">
+				<tbody>
+					<tr v-for="field,i in info" :key="i">
+						<td>{{ field.label }}</td>
+						<td>{{ field.value }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import AppField from './components/Field'
+import AppProgress from './components/Progress'
+
+export default {
+	components: { AppField, AppProgress },
+	data: () => ({
+		info: [
+			{
+				label: 'Name',
+				value: '',
+				pattern: /^[a-zA-Z ]{2,30}$/
+			},
+			{
+				label: 'Phone',
+				value: '',
+				pattern: /^[0-9]{7,14}$/
+			},
+			{
+				label: 'Email',
+				value: '',
+				pattern: /.+/
+			},
+			{
+				label: 'Some Field 1',
+				value: '',
+				pattern: /.+/
+			},
+			{
+				label: 'Some Field 2',
+				value: '',
+				pattern: /.+/
+			}
+		],
+		formDone: false
+	}),
+	computed: {
+		fieldDone(){
+			// return this.info.reduce((total, field) => total + (field.valid ? 1 : 0), 0)
+			return this.info.filter(field => field.valid).length;
+		},
+		formReady(){
+			return this.fieldDone >= this.info.length;
+		},
+		progressStyles(){
+			let rel = this.fieldDone / this.info.length * 100;
+			return { width: rel + '%' };
+		}
+	},
+	methods: {
+		onUpdate(i, val){
+			let field = this.info[i];
+			field.value = val.trim();
+			field.valid = field.pattern.test(field.value);
+		},
+		sendForm(){
+			if(this.formReady){
+				this.formDone = true;
+			}
+		}
+	},
+	created(){
+		this.info.forEach(field => {
+			field.valid = false;
+		})
+	}
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+</script>
